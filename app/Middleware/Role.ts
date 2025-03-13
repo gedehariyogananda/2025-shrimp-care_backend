9f6e5fcfbe989598a9e3d4a44bebcb765f860f4f
gedehariyogananda <gedehariyoga@gmail.com>
2025-03-13 22:29:26 +0700

    add: setup roles middleware int

--- app/Middleware/Role.ts
+++ app/Middleware/Role.ts
@@ -1,21 +1,24 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import AuthException from 'App/Exceptions/AuthException'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import AuthException from "App/Exceptions/AuthException";
 
 export default class Role {
-  public async handle({ auth }: HttpContextContract, next: () => Promise<void>, allowedRoles: string[]) {
+  public async handle(
+    { auth }: HttpContextContract,
+    next: () => Promise<void>,
+    allowedRoles: string[]
+  ) {
     if (allowedRoles.length == 0) {
-      await next()
+      await next();
     } else {
       try {
-        const roleId = (await auth.use('api').authenticate()).roles
-        const role = await this.roleService.find(roleId)        
-        if (allowedRoles.includes(role.code)) {
-          await next()
+        const userRole = auth.user?.roles || "";
+        if (allowedRoles.includes(userRole)) {
+          await next();
         } else {
-          throw new AuthException('Forbidden!', 403, 'E_UNAUTHORIZED_ACCESS')
+          throw new AuthException("Forbidden!", 403, "E_UNAUTHORIZED_ACCESS");
         }
       } catch (e) {
-        throw new AuthException('Forbidden!', 403, 'E_UNAUTHORIZED_ACCESS')
+        throw new AuthException("Forbidden!", 403, "E_UNAUTHORIZED_ACCESS");
       }
     }
   }
--- start/routes/user/users.ts
+++ start/routes/user/users.ts
@@ -4,4 +4,4 @@ Route.group(function () {
   Route.delete("/", "User/UserController.destroyAll").as("users.destroyAll");
 }).prefix("users");
 Route.resource("users", "User/UserController").apiOnly();
-Route.get("user/profile", "User/UserController.getProfile").middleware("auth");
+Route.get("user/profile", "User/UserController.getProfile").middleware(['auth', 'role:user']);
